8cd9eebb496a836d073c45306e68d076fa23e7f6
Harry Chuang <hanchangchuang@gmail.com>
2022-02-28 09:23:29 -0800

    Added a POST endpoint for Person

--- src/main/java/com/harrychuang/demo/api/PersonController.java
+++ src/main/java/com/harrychuang/demo/api/PersonController.java
@@ -4,10 +4,14 @@ import com.harrychuang.demo.model.Person;
 import com.harrychuang.demo.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController // NOTE-HC Delcare this class as a REST API Controller for Spring Boot
+@RequestMapping("api/v1/person") // NOTE-HC Map a specific route to this controller
+@RestController // NOTE-HC Declare this class as a REST API Controller for Spring Boot
 public class PersonController {
+
   private final PersonService personService;
 
   @Autowired // NOTE-HC Automatically inject a PersonService class into this class
@@ -15,8 +19,17 @@ public class PersonController {
     this.personService = personService;
   }
 
+  // @formatter:off
+  /**
+   * NOTE-HC
+   *  - `@PostMapping`: declare this method as a post endpoint
+   *  - `@RequestBody`: declare this parameter as the body of the request.
+   *      Since Person's constructor params are already declared `@JsonProperty("XXX")`,
+   *      Spring-boot can automatically map the JSON properties to the class properties.
+   */
+  // @formatter:on
   @PostMapping
-  public void addPerson(Person person) {
+  public void addPerson(@RequestBody Person person) {
     personService.addPerson(person);
   }
 }
--- src/main/java/com/harrychuang/demo/model/Person.java
+++ src/main/java/com/harrychuang/demo/model/Person.java
@@ -1,12 +1,18 @@
 package com.harrychuang.demo.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.UUID;
 
 public class Person {
+
   private final UUID id;
   private final String name;
 
-  public Person(UUID id, String name) {
+  /**
+   * NOTE-HC Map these params to a json object { "id": "893d4e11-1fe0-4f85-8bd6-35ea91a35f55",
+   * "name": "James Bond" }
+   **/
+  public Person(@JsonProperty("id") UUID id, @JsonProperty("name") String name) {
     this.id = id;
     this.name = name;
   }
