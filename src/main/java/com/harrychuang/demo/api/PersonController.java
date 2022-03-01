36ac18bb97de48a0c577e490b730ba1ecff410b1
Harry Chuang <hanchangchuang@gmail.com>
2022-03-01 09:42:51 -0800

    Added @NonNull, @Validate, @NotBlank to validate Person model on Request Body. Note validate is another package.

--- src/main/java/com/harrychuang/demo/api/PersonController.java
+++ src/main/java/com/harrychuang/demo/api/PersonController.java
@@ -4,7 +4,9 @@ import com.harrychuang.demo.model.Person;
 import com.harrychuang.demo.service.PersonService;
 import java.util.List;
 import java.util.UUID;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -34,7 +36,7 @@ public class PersonController {
    */
   // @formatter:on
   @PostMapping
-  public void addPerson(@RequestBody Person person) {
+  public void addPerson(@Valid @NonNull @RequestBody Person person) {
     personService.addPerson(person);
   }
 
@@ -54,8 +56,12 @@ public class PersonController {
     personService.deletePerson(id);
   }
 
+  // NOTE-HC
+  //  - @Valid: Spring-boot can validate a model after binding user input to it.
+  //  - @NonNull: Spring-boot can ensure a parameter is not null
   @PutMapping(path = "{id}")
-  public void updatePersonById(@PathVariable("id") UUID id, @RequestBody Person personToUpdate) {
+  public void updatePersonById(
+      @PathVariable("id") UUID id, @Valid @NonNull @RequestBody Person personToUpdate) {
     personService.updatePerson(id, personToUpdate);
   }
 }
--- src/main/java/com/harrychuang/demo/model/Person.java
+++ src/main/java/com/harrychuang/demo/model/Person.java
@@ -2,11 +2,15 @@ package com.harrychuang.demo.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.UUID;
+import javax.validation.constraints.NotBlank;
 
 public class Person {
 
   private final UUID id;
-  private final String name;
+
+  // NOTE-HC We are not using @NotNull because it would still permit empty string
+  // This tells Spring-Boot when we pass in Person in request body, name cannot be null or empty str
+  @NotBlank private final String name;
 
   /**
    * NOTE-HC Map these params to a json object { "id": "893d4e11-1fe0-4f85-8bd6-35ea91a35f55",
